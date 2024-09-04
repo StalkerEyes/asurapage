43756b1df4ba1c94ce1b273960367f8590ab1e73
StalkerEyes <43144435+StalkerEyes@users.noreply.github.com>
2024-09-04 13:55:06 -0600

    Update index.php
    correcion de ;

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-echo "<h1> Creacion de archivo<h1>"
-$file = open("logs/logs.txt", "a")
+echo "<h1> Creacion de archivo<h1>";
+$file = open("logs/logs.txt", "a");
   
 fputs($file, "prueba de creacion de archivo");
 
