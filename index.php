1b962a83b353d1f54da856bc5c2177a2f812c4b4
StalkerEyes <43144435+StalkerEyes@users.noreply.github.com>
2024-09-04 13:49:05 -0600

    Update index.php
    se coloca codigo de creacion de archivo

--- index.php
+++ index.php
@@ -1,46 +1,7 @@
 <?php
-echo "HOLIS";
-  $fh = fopen("prueba.txt", 'w') or die("Se produjo un error al crear el archivo");
+echo "<h1> Creacion de archivo<h1>"
+$file = open("logs/logs.txt", "a")
   
-  $texto = <<<_END
-Comienza con Linea 1
-Linea 2
-Termina con Linea 3
-_END;
-  
-  fwrite($fh, $texto) or die("No se pudo escribir en el archivo");
-  
-  fclose($fh);
-  
-  echo "Se ha escrito sin problemas";
-?>
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-<?php
-  $fh = fopen("prueba.txt", 'w') or die("Se produjo un error al crear el archivo");
-  
-  $texto = <<<_END
-Comienza con Linea 1
-Linea 2
-Termina con Linea 3
-_END;
-  
-  fwrite($fh, $texto) or die("No se pudo escribir en el archivo");
-  
-  fclose($fh);
-  
-  echo "Se ha escrito sin problemas";
+fputs($file, "prueba de creacion de archivo");
+
 ?>
