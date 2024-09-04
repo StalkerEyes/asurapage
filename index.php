f877447acb3eba015809396f427141e23951c1bb
StalkerEyes <43144435+StalkerEyes@users.noreply.github.com>
2024-09-04 14:03:42 -0600

    Update index.php
    codigo corregido

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 echo "<h1> Creacion de archivo<h1>";
-$file = open("logs.txt", "a");
+
+if(!is_dir("logs")){
+	mkdir("logs", 0777, true);
+}
+
+$file = fopen("logs/logs.txt", "a");
   
 fputs($file, "prueba de creacion de archivo");
 
